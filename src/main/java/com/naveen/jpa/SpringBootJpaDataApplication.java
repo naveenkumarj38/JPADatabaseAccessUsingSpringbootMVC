9ad4aab2c26f01b4349e157fe1588037d8a0a17d
naveen kumar <naveenkumarj38@gmail.com>
2017-02-04 15:59:20 +0530

    Add files via upload

---
+++ src/main/java/com/naveen/jpa/SpringBootJpaDataApplication.java
@@ -0,0 +1,12 @@
+package com.naveen.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaDataApplication.class, args);
+	}
+}
